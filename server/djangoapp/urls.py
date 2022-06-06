5fbc7e72719e324640f2a65c789856b52411f772
Nasser Abdullah <nalshlwan@kacst.edu.sa>
2022-06-06 06:12:56 +0000

    week1 finish

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,12 +10,15 @@ urlpatterns = [
     # name the URL
 
     # path for about view
+    path(route='about', view=views.about, name='about'),
 
     # path for contact us view
+    path(route='contact', view=views.contact, name='contact'),
 
     # path for registration
 
     # path for login
+    path(route='login/', view=views.login_request, name='login'),
 
     # path for logout
 
